c574782c093ee62e9d0e6dd0ba8b9b12f498b4b2
kawsar00 <kawsarmahmud358@gmail.com>
2020-10-31 00:24:42 +0600

    form updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -4,19 +4,20 @@ import './Contact.css';
 const Contact = () => {
   return (
     <div id="contact" className="">
-      <div className="row contact-container p-5">
+      <h1 className='text-center font-weight-bold'>CONTACT</h1>
+      <div className="row p-5">
+      
         <div className="col-md-12 container-fluid px-4">
-          <div className="col-md-6">
+          <div className="col-md-6 shadow-lg p-5 contact-style">
             <form>
               <div class="form-group d-flex justify-content-center">
-                <input type="text" class="form-control" name="name" autocomplete="off" id="exampleInputName" placeholder="Enter your name" required="" />
-              </div>
-
-              <div class="form-group d-flex justify-content-center">
-                <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email" autocomplete="off" required="" />
+                <input type="text" class="form-control" name="name" autocomplete="off" id="exampleInputName" placeholder="Enter your name" required />
               </div>
               <div class="form-group d-flex justify-content-center">
-                <textarea class="form-control" name="message" rows="3" placeholder="Your Message" required=""></textarea>
+                <input type="email" class="form-control" name="email" id="exampleInputEmail1" placeholder="Email" autocomplete="off" required />
+              </div>
+              <div class="form-group d-flex justify-content-center">
+                <textarea class="form-control" name="message" rows="3" placeholder="Your Message" required></textarea>
               </div>
               <div class="">
               <button type="submit" className="btn btn-bg text-white">SUBMIT</button>
