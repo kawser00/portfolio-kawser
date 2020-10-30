f9c95f1b6bf232f34991874d71003178ddaab468
kawsar00 <kawsarmahmud358@gmail.com>
2020-10-30 23:28:42 +0600

    project site link added

--- src/Components/ProjectCard/ProjectCard.jsx
+++ src/Components/ProjectCard/ProjectCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import './ProjectCard.css'; 
+import './ProjectCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, } from '@fortawesome/free-brands-svg-icons';
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 const ProjectCard = (props) => {
-  const { title, description, img } = props.singleProject
+  const { title, description, img, github, live } = props.singleProject
   return (
     <div className="col-md-4 col-sm-12 serviceCard">
       <div className="single-item my-4">
@@ -11,7 +15,10 @@ const ProjectCard = (props) => {
           <div className="card-body">
             <h4 className="card-title font-weight-bold">{title}</h4>
             <p className="card-text">{description}</p>
-            <br />
+            <div className="pt-2">
+              <span><a className="text-color mr-3 " href={github} target="/blank"><FontAwesomeIcon className="icon-style" icon={faGithub} /> <span></span></a></span>
+              <span><a className="text-color" href={live} target="/blank"><FontAwesomeIcon className="icon-style" icon={faExternalLinkAlt} /> <span></span></a></span>
+            </div>
           </div>
         </div>
       </div>
--- src/Components/data/ProjectData/ProjectData.jsx
+++ src/Components/data/ProjectData/ProjectData.jsx
@@ -8,39 +8,25 @@ const ProjectData = [
     title: 'Creative Agency',
     description: "It's a full stack single page web application where people can order any software service and manage it from dashboard. An Admin can access data, add new service, make new admin from admin dashboard. ",
     img: creative,
+    github: "https://github.com/kawsar00/creative-agency",
+    live: "https://creative-agency-5d0a0.web.app/"
   },
   {
     id: 'PJ2002',
     title: 'Volunteer Network',
     description: "It’s a full-stack responsive web application,People can select and register volunteer task ",
     img: volunteer,
+    github: "https://github.com/kawsar00/volunteer-network",
+    live: "https://volunteer-network-app.netlify.app/"
   },
   {
     id: 'PJ2003',
     title: 'Travel Guru',
     description: "It’s a single page hotel booking web application, user friendly. People can book his/her desirable location and hotel rooms as well. ",
     img: travel,
+    github: "https://github.com/kawsar00/travel-guru",
+    live: "https://travel-guru-site.netlify.app/"
   },
-  {
-    id: 'PJ2004',
-    title: 'Volunteer Network',
-    description: "It’s a full-stack responsive web application,People can select and register volunteer task ",
-    img: volunteer,
-  },
-  {
-    id: 'PJ2005',
-    title: 'Travel Guru',
-    description: "It’s a single page hotel booking web application, user friendly. People can book his/her desirable location and hotel rooms as well. ",
-    img: travel,
-  },
-  {
-    id: 'PJ2006',
-    title: 'Creative Agency',
-    description: "It's a full stack single page web application where people can order any software service and manage it from dashboard. An Admin can access data, add new service, make new admin from admin dashboard. ",
-    img: creative,
-  },
-
-  
 ]
 
 export default ProjectData;
