5e71da07243c5ca1dea5384388fe12da2b7c3089
kawsar00 <kawsarmahmud358@gmail.com>
2020-10-31 23:08:40 +0600

    site updated

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Contact.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faGithub, faLinkedin, faLinkedinIn, faMedium, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faMapMarker, faMapMarkerAlt, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 const Contact = () => {
   return (
     <div id="contact" className="">
       <h1 className='text-center font-weight-bold'>CONTACT</h1>
       <h5 className='text-center text-color font-weight-bold'>Have a question or want to work together?</h5>
-
-      <div className="row p-5">
-
-        <div className="col-md-12 container-fluid px-4">
-          <div className="col-md-6 shadow-lg p-5 contact-style">
+        <div className="container-fluid">
+          <div className="row p-3 d-flex justify-content-between">
+          <div className="col-md-6 col-sm-12 shadow-lg p-5 contact-style">
             <h4 className='text-center font-weight-bold pb-4'>Get In Tough</h4>
             <form>
               <div class="form-group d-flex justify-content-center">
@@ -27,8 +29,44 @@ const Contact = () => {
               </div>
             </form>
           </div>
-        </div>
-        <div className="col-md-12">
+          <div className="col-md-6 col-sm-12">
+            <div className="contact-info">
+              <div className="mb-2">
+                <span className="mr-5"><FontAwesomeIcon className="text-color" icon={faMapMarkerAlt} /></span>
+                <span>Mirpur 12, Dhaka, Bangladesh</span>
+              </div>
+              <div className="mb-2">
+                <span className="mr-5"><FontAwesomeIcon className="text-color" icon={faPhoneAlt} /></span>
+                <span>+880-1710-302513</span>
+              </div>
+              <div className="mb-2">
+                <span className="mr-5"><FontAwesomeIcon className="text-color" icon={faEnvelope} /></span>
+                <span>kawser.ahmed9009@gmail.com</span>
+              </div>
+            </div>
+            <hr className="btn-bg my-5" />
+            <div className="social-icon">
+              <ul className="d-flex list-unstyled">
+                <li>
+                  <a className=" social-icon-style" href="https://web.facebook.com/kawsar.mahmud00/" target="/_blank"><FontAwesomeIcon  icon={faFacebook} /></a>
+                </li>
+                <li>
+                  <a className=" social-icon-style" href="https://twitter.com/Kawser12759188" target="/_blank"><FontAwesomeIcon  icon={faTwitter} /></a>
+                </li>
+                <li>
+                  <a className=" social-icon-style" href="https://www.linkedin.com/in/kawsarmahmud/" target="/_blank"><FontAwesomeIcon  icon={faLinkedinIn} /></a>
+                </li>
+                <li>
+                  <a className=" social-icon-style" href="https://github.com/kawsar00" target="/_blank"><FontAwesomeIcon  icon={faGithub} /></a>
+                </li>
+                <li>
+                  <a className=" social-icon-style" href="https://medium.com/@kawsarmahmud358" target="/_blank"><FontAwesomeIcon  icon={faMedium} /></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+         
+          
         </div>
       </div>
       <footer className="copyRight text-center bg-dark p-4 text-white">
